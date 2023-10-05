c175984304eadb6536317d9a532811f3b806b599
leszczynskimaciek98 <maciek.leszcz1998@gmail.com>
2023-10-06 00:34:21 +0200

    Added a new functionality allowing the user to jump back to the Main Activity by using Right Swipe

--- app/src/main/java/com/example/test_kotlin_images/messages_activity.kt
+++ app/src/main/java/com/example/test_kotlin_images/messages_activity.kt
@@ -1,8 +1,10 @@
 package com.example.test_kotlin_images
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.abs
@@ -49,10 +51,12 @@ class messages_activity : AppCompatActivity(), GestureDetector.OnGestureListener
                 val valueX:Float = x2-x1
                 if(abs(valueX) > MainActivity.MIN_DISTANCE)
                 {
-                    //Right swipe
+                    //Right swipe to the MainActivity
                     if(x2 > x1)
                     {
                         Toast.makeText(this,"Right swipe", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
 
                     }
                     //Left swipe
