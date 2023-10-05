2f6de34ad73d6dfd95d59783191b9b99b89337af
leszczynskimaciek98 <maciek.leszcz1998@gmail.com>
2023-10-05 23:58:15 +0200

    Added a new test functionality for detecting left/right swap

--- app/src/main/java/com/example/test_kotlin_images/MainActivity.kt
+++ app/src/main/java/com/example/test_kotlin_images/MainActivity.kt
@@ -3,20 +3,104 @@ package com.example.test_kotlin_images
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
+import kotlin.math.abs
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener{
+
+    lateinit var gestureDetector: GestureDetector
+    var x2:Float = 0.0f
+    var x1:Float = 0.0f
+    var y2:Float = 0.0f
+    var y1:Float = 0.0f
+
+    companion object{
+        const val MIN_DISTANCE = 150
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+        //button initiation
         val messageButton = findViewById<ImageButton>(R.id.message_button)
         messageButton.setOnClickListener(View.OnClickListener {
 
             val intent = Intent(this, messages_activity::class.java)
             startActivity(intent)
         })
+        gestureDetector = GestureDetector(this, this)
+    }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        if (event != null) {
+            gestureDetector.onTouchEvent(event)
+        }
+
+        when(event?.action){
+            //start to swipe
+            0->
+            {
+                x1 = event.x
+                y1 = event.y
+            }
+            //end to swipe
+            1->
+            {
+                x2 = event.x
+                y2 = event.y
+                val valueX:Float = x2-x1
+                if(abs(valueX) > MIN_DISTANCE)
+                {
+                    if(x2 > x1)
+                    {
+                        Toast.makeText(this,"Right swipe", Toast.LENGTH_SHORT).show()
+                    }
+                    else
+                    {
+                        Toast.makeText(this, "Left swipe", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }
+        }
+
+        return super.onTouchEvent(event)
+    }
+
+    //Not needed right now
+    override fun onDown(p0: MotionEvent): Boolean {
+        //TODO("Not yet implemented")
+        return false
+    }
+
+    override fun onShowPress(p0: MotionEvent) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent): Boolean {
+        //TODO("Not yet implemented")
+        return false
+    }
+
+    override fun onScroll(p0: MotionEvent, p1: MotionEvent, p2: Float, p3: Float): Boolean {
+        //TODO("Not yet implemented")
+        return false
+    }
+
+    override fun onLongPress(p0: MotionEvent) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun onFling(p0: MotionEvent, p1: MotionEvent, p2: Float, p3: Float): Boolean {
+        //TODO("Not yet implemented")
+        return false
     }
 }
